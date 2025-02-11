a1a2c2d272b61cc47f6d1e4db1a633d20bb7d859
Pedrootarga <miranda_785@hotmail.com>
2025-02-11 20:19:52 -0300

    11.02

--- Player/player_scene.gd
+++ Player/player_scene.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 func _process(delta: float) -> void:
 	var direction : float = 0.0
 	if Input.is_action_pressed("left"):
-		direction = direction - 1.0
+		direction = direction - 5.0
 	if Input.is_action_pressed("right"):
-		direction = direction + 1.0
+		direction = direction + 5.0
 	apply_central_impulse(Vector2(direction,0))
