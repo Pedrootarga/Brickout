5a25aac84fea9cf5a794d9885e13d3072ff53275
Pedrootarga <miranda_785@hotmail.com>
2025-02-12 17:53:29 -0300

    12.2

--- Player/player_scene.gd
+++ Player/player_scene.gd
@@ -3,7 +3,12 @@ extends RigidBody2D
 func _process(delta: float) -> void:
 	var direction : float = 0.0
 	if Input.is_action_pressed("left"):
-		direction = direction - 5.0
+		direction = direction - 1.0
 	if Input.is_action_pressed("right"):
-		direction = direction + 5.0
-	apply_central_impulse(Vector2(direction,0))
+		direction = direction + 1.0
+	apply_central_impulse(15*Vector2(direction,0))
+	if direction != 0:
+		if direction < 0 and $Sprite2DPlayer.scale.x > 0:
+			$Sprite2DPlayer.scale.x = -1.5
+		if direction > 0 and $Sprite2DPlayer.scale.x < 0:
+			$Sprite2DPlayer.scale.x = 1.5
