8ae8215c1aabaa60365b945db7bec3f682bbb6b9
Pedrootarga <miranda_785@hotmail.com>
2025-02-18 22:45:29 -0300

    18.02 v3

--- Brick/brick_spawner.gd
+++ Brick/brick_spawner.gd
@@ -6,37 +6,35 @@ extends Node2D
 @export var num_bricks: int = 3
 @export var max_attempts = 10
 
-var occupied_positions = []
-
 func _ready():
 	if brick_scene:
 		var screen_width = get_viewport_rect().size.x
-		
+		var main_scene = get_tree().current_scene
+		var bricks_node = main_scene.get_node("Node2DBricks") #Obtém o nó Node2DBricks
+
 		for i in range(num_bricks):
 			var size = randf_range(min_size, max_size)
 			var random_size = Vector2(size, size)
 			var brick = brick_scene.instantiate()
-			brick.set_scale(random_size / brick.get_child(0).get_texture().get_size()) 
-			
+			brick.set_scale(random_size / brick.get_child(0).get_texture().get_size())
+
 			var pos_x
 			var attempts = 0
 			var valid_position = false
-			
+
 			while attempts < max_attempts and not valid_position:
 				pos_x = randf_range(random_size.x, screen_width - random_size.x)
 				valid_position = true
-				
-				for occupied in occupied_positions:
-					var occupied_x = occupied["position"]
-					var occupied_size = occupied["size"]
-					
-					if abs(pos_x - occupied_x) < (random_size.x / 2 + occupied_size / 2):
-						valid_position = false
-						break
-						
+
+				for child in bricks_node.get_children(): #Utiliza o nó Node2DBricks para detectar colisões
+					if child is StaticBody2D:
+						var child_size = child.get_scale().x * child.get_node("Sprite2D").texture.get_size().x
+						if abs(pos_x - child.position.x) < (random_size.x / 2 + child_size / 2):
+							valid_position = false
+							break
+
 				attempts += 1
-				
+
 			if valid_position:
-					occupied_positions.append({"position": pos_x, "size": random_size.x})
-					add_child(brick)
-					brick.position = Vector2(pos_x, random_size.y/2) 
+				bricks_node.add_child(brick) #Adiciona o bloco ao nó Node2DBricks
+				brick.position = Vector2(pos_x, random_size.y / 2)
